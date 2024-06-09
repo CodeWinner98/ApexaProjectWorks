d957f3df0717a361c2105b3a69411386fd549601
CodeWinner98 <rmviraj1998bandara@gmail.com>
2024-06-09 10:42:00 +0530

    Update App.js

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Sign from './components/Sign';
+import Pricing from './components/Pricing';
+import Gallery from './components/Gallery';
+import Create from './components/Create';
+import About from './components/About';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign" element={<Sign />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
